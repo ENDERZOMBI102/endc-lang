2ecdd5de46754ab151d30810637269ce30424853
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-10-26 10:21:44 +0200

    removed two casts moved argument parsing to its own module

---
+++ src/cli.py
@@ -0,0 +1,87 @@
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Optional, Literal
+
+parser = ArgumentParser(
+	prog='compiler.py' if getattr(sys, 'frozen', False) else 'endcc',
+	description='End C Compiler'
+)
+parser.add_argument(
+	'-f',
+	'--file',
+	help='Input file',
+	action='store',
+	type=Path,
+	dest='file'
+)
+parser.add_argument(
+	'-b',
+	'--backend',
+	help='Must be one of "inter", "llvm", "wasm", "py", "jvm", "neko" or "js"',
+	action='store',
+	type=str,
+	dest='backend'
+)
+parser.add_argument(
+	'-c',
+	'--config',
+	help='Sets the config file to the provided path',
+	action='store',
+	type=Path,
+	default='.endcc.json',
+	dest='configFile'
+)
+parser.add_argument(
+	'-a',
+	'--aftercomp',
+	help='Sets the python script exececuted after compile to the provided file',
+	action='store',
+	type=Path,
+	dest='postCompileScript'
+)
+parser.add_argument(
+	'-v',
+	'--verbosity',
+	help='Sets the verbosity of the log (0: everything 1: warns+errors 2: errors)',
+	action='store',
+	type=int,
+	dest='verboseLevel'
+)
+parser.add_argument(
+	'--backend-info',
+	help='Used to get information about available backends',
+	action='store_true',
+	default=False,
+	dest='showBackendHelp'
+)
+parser.add_argument(
+	'--interactive',
+	help='Used to run the interpreter in interactive mode',
+	action='store_true',
+	default=False,
+	dest='interactiveMode'
+)
+parser.add_argument(
+	'--exit-on-error',
+	help='Makes the interpreter exits when a implementation error occurs',
+	action='store_true',
+	default=False,
+	dest='exitOnImplementationError'
+)
+
+
+class Arguments:
+	file: Path
+	backend: Literal['inter', 'llvm', 'wasm', 'py', 'jvm', 'neko', 'js']
+	showBackendHelp: bool
+	configFile: Path
+	postCompileScript: Optional[Path]
+	interactiveMode: bool
+	exitOnImplementationError: bool
+	# 0: everything 1: warns up 2: only errors
+	verboseLevel: int
+
+
+# cli arguments
+args: Arguments = parser.parse_args( sys.argv[ 1: ] )  # type: ignore
--- src/compiler.py
+++ src/compiler.py
@@ -2,91 +2,18 @@ import os
 import sys
 from json import loads
 from pathlib import Path
-from argparse import ArgumentParser
-from typing import Literal, cast, TextIO, Optional, Union, Any
+from typing import cast, TextIO, Union
 from time import time
 from importlib import import_module
 
-from backend import Backend, BACKENDS
+from backend import BACKENDS
 import ast_.parser
 import tokenizer
+from cli import args
 from utils import ExitError
-
-parser = ArgumentParser(
-	prog='compiler.py' if getattr(sys, 'frozen', False) else 'endcc',
-	description='End C Compiler'
-)
-parser.add_argument(
-	'-f',
-	'--file',
-	help='Input file',
-	action='store',
-	type=Path,
-	dest='file'
-)
-parser.add_argument(
-	'-b',
-	'--backend',
-	help='Must be one of "inter", "llvm", "wasm", "py", "jvm", "neko" or "js"',
-	action='store',
-	type=str,
-	dest='backend'
-)
-parser.add_argument(
-	'-c',
-	'--config',
-	help='Sets the config file to the provided path',
-	action='store',
-	type=Path,
-	default='.endcc.json',
-	dest='configFile'
-)
-parser.add_argument(
-	'-a',
-	'--aftercomp',
-	help='Sets the python script exececuted after compile to the provided file',
-	action='store',
-	type=Path,
-	dest='postCompileScript'
-)
-parser.add_argument(
-	'-v',
-	'--verbosity',
-	help='Sets the verbosity of the log (0: everything 1: warns+errors 2: errors)',
-	action='store',
-	type=int,
-	dest='verboseLevel'
-)
-parser.add_argument(
-	'--backend-info',
-	help='Used to get information about available backends',
-	action='store_true',
-	default=False,
-	dest='showBackendHelp'
-)
-parser.add_argument(
-	'--interactive',
-	help='Used to run the interpreter in interactive mode',
-	action='store_true',
-	default=False,
-	dest='interactiveMode'
-)
-
-
-class Arguments:
-	file: Path
-	backend: Literal['inter', 'llvm', 'wasm', 'py', 'jvm', 'neko', 'js']
-	showBackendHelp: bool
-	configFile: Path
-	postCompileScript: Optional[Path]
-	interactiveMode: bool
-	# 0: everything 1: warns up 2: only errors
-	verboseLevel: int
 
 
 def main() -> int:
-	# cli arguments
-	args: Arguments = cast( Arguments, parser.parse_args( sys.argv[ 1: ] ) )
 	# backend help (--backed-info)
 	if args.showBackendHelp:
 		txt = 'note: backends with * are not yet available\n'
@@ -119,7 +46,7 @@ def main() -> int:
 	try:
 		# interactive mode
 		if args.interactiveMode:
-			return cast( Any, import_module( 'backend.interpreter.interactive' ) ).interactiveMain()
+			return import_module( 'backend.interpreter.interactive' ).interactiveMain()  # type: ignore
 
 		if not args.file.exists():
 			log(2, f'[ERROR] File {args.file} not found.', sys.stderr)
@@ -145,7 +72,7 @@ def main() -> int:
 			return 1
 
 		log(0, f'[INFO] Executing backend "{backend.name}"..')
-		exitCode = cast( Backend, import_module( backend.pkg ) ).backendMain(ast)
+		exitCode: int = import_module( backend.pkg ).backendMain(ast)  # type: ignore
 
 		if args.postCompileScript:
 			if not args.postCompileScript.exists():
